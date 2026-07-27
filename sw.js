d0274acfacaa1084f0fd0ef4c60142b153f6930c
isgdeniz-ai <isg.deniz@gmail.com>
2026-07-27 09:15:23 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Word Memorizer - service worker
 // HTML icin "once ag, olmazsa onbellek" (guncellemeler hemen gelsin)
 // Diger dosyalar icin "once onbellek" (hizli acilsin)
-const CACHE = 'wordmem-v25';
+const CACHE = 'wordmem-v27';
 const ASSETS = [
   './',
   './index.html',
