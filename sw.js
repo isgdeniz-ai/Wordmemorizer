c0148e60764fe8770d803d9ebcbd73c20da61f4c
Word memorizer <isg.deniz@gmail.com>
2026-08-05 22:09:35 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Word Memorizer - service worker
 // HTML icin "once ag, olmazsa onbellek" (guncellemeler hemen gelsin)
 // Diger dosyalar icin "once onbellek" (hizli acilsin)
-const CACHE = 'wordmem-v63';
+const CACHE = 'wordmem-v74';
 const ASSETS = [
   './',
   './index.html',
